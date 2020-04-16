409ff1a5bb195fb76713c301aa4d47bc26e199fd
Tim Shaker <tim@tmshkr.com>
2020-04-16 14:03:19 -0700

    update schema

--- data/migrations/20200416121323_recipe-book.js
+++ data/migrations/20200416121323_recipe-book.js
@@ -1,19 +1,16 @@
 exports.up = function (knex) {
   return (
     knex.schema
-      // ingredients
-      .createTable("ingredients", (tbl) => {
-        tbl.increments("id");
-        tbl.string("name").notNullable();
-        tbl.string("quantity").notNullable();
-      })
       // recipes
       .createTable("recipes", (tbl) => {
         tbl.increments("id");
         tbl.string("name").notNullable();
       })
-      // ingredient_list
-      .createTable("ingredient_list", (tbl) => {
+      // ingredients
+      .createTable("ingredients", (tbl) => {
+        tbl.string("name").notNullable();
+        tbl.float("quantity").notNullable();
+        tbl.string("unit").notNullable();
         tbl
           .integer("recipe_id")
           .unsigned()
@@ -21,35 +18,17 @@ exports.up = function (knex) {
           .references("recipes.id")
           .onUpdate("CASCADE")
           .onDelete("CASCADE");
-        tbl
-          .integer("ingredient_id")
-          .unsigned()
-          .notNullable()
-          .references("ingredients.id")
-          .onUpdate("CASCADE")
-          .onDelete("CASCADE");
-        tbl.primary(["recipe_id", "ingredient_id"]);
-      })
-      // steps
-      .createTable("steps", (tbl) => {
-        tbl.increments("id");
-        tbl.text("description").notNullable();
+        tbl.primary(["recipe_id", "name"]);
       })
       // instructions
       .createTable("instructions", (tbl) => {
         tbl.integer("step_number");
+        tbl.string("description").notNullable();
         tbl
           .integer("recipe_id")
           .unsigned()
           .notNullable()
           .references("recipes.id")
-          .onUpdate("CASCADE")
-          .onDelete("CASCADE");
-        tbl
-          .integer("step_id")
-          .unsigned()
-          .notNullable()
-          .references("step.id")
           .onUpdate("CASCADE")
           .onDelete("CASCADE");
         tbl.primary(["recipe_id", "step_number"]);
@@ -59,9 +38,7 @@ exports.up = function (knex) {
 
 exports.down = function (knex) {
   return knex.schema
-    .dropTableIfExists("ingredients")
-    .dropTableIfExists("ingredient_list")
     .dropTableIfExists("recipes")
-    .dropTableIfExists("steps")
+    .dropTableIfExists("ingredients")
     .dropTableIfExists("instructions");
 };
