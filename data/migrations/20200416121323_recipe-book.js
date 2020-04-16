aa56928e21568f8ded0c7265ba05a62dfc226998
Tim Shaker <tim@tmshkr.com>
2020-04-16 12:54:54 -0700

    initial schema

--- data/migrations/20200416121323_recipe-book.js
+++ data/migrations/20200416121323_recipe-book.js
@@ -1,8 +1,55 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return (
+    knex.schema
+      // ingredients
+      .createTable("ingredients", (tbl) => {
+        tbl.increments("id");
+        tbl.string("name").notNullable();
+        tbl.string("quantity").notNullable();
+      })
+      // recipes
+      .createTable("recipes", (tbl) => {
+        tbl.increments("id");
+        tbl.string("name").notNullable();
+        tbl
+          .integer("ingredient_id")
+          .unsigned()
+          .notNullable()
+          .references("ingredients.id")
+          .onUpdate("CASCADE")
+          .onDelete("CASCADE");
+      })
+      // steps
+      .createTable("steps", (tbl) => {
+        tbl.increments("id");
+        tbl.text("description").notNullable();
+      })
+      // instructions
+      .createTable("instructions", (tbl) => {
+        tbl.integer("step_number");
+        tbl
+          .integer("recipe_id")
+          .unsigned()
+          .notNullable()
+          .references("recipes.id")
+          .onUpdate("CASCADE")
+          .onDelete("CASCADE");
+        tbl
+          .integer("step_id")
+          .unsigned()
+          .notNullable()
+          .references("step.id")
+          .onUpdate("CASCADE")
+          .onDelete("CASCADE");
+        tbl.primary(["recipe_id", "step_number"]);
+      })
+  );
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("recipes")
+    .dropTableIfExists("steps")
+    .dropTableIfExists("instructions");
 };
--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: "./recipe-book.db3",
+      filename: "./data/recipe-book.db3",
     },
     migrations: {
       directory: "./data/migrations",
